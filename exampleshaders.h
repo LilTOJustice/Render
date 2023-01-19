f9384146e76f028b97c47fddc0435b8b3f4f8785
Nicholas Muia <muian@NicksPc>
2023-01-19 00:46:56 -0800

    Modified frag shader parameter names

--- exampleshaders.h
+++ exampleshaders.h
@@ -1,8 +1,6 @@
 #include <complex>
 #include <cmath>
 
-#include <iostream>
-
 #include "shader.h"
 
 const int MAXITS = 50;
@@ -43,8 +41,8 @@ int multi(const std::complex<long double> &c, const long double exponent)
 FragShader mandelbrot =
 Frag
 {
-    fVec2 st = FragCoord/Res;
-    st.x *= 1.*Res.x/Res.y;
+    fVec2 st = fragcoord/res;
+    st.x *= 1.*res.x/res.y;
     st = (st-fVec2(1.25, .5))/(.1*(sin(time) + .7) + .3);
     std::complex<long double> c(st.x, st.y);
     long double mandelval = 255.*(MAXITS-mandel(c))/MAXITS;
@@ -54,8 +52,8 @@ Frag
 FragShader multibrot =
 Frag
 {
-    fVec2 st = FragCoord/Res;
-    st.x *= 1.*Res.x/Res.y;
+    fVec2 st = fragcoord/res;
+    st.x *= 1.*res.x/res.y;
     st = (st-fVec2(1.25, .5))/.3;
     std::complex<long double> c(st.x, st.y);
     long double mandelval = 255.*(MAXITS-multi(c, 3*cos(time)+4))/MAXITS;
--- shader.h
+++ shader.h
@@ -5,7 +5,7 @@
 #include "linalg.h"
 
 // Frag shaders
-#define FRAGPARAMLIST RGBRef out, const uVec2 &FragCoord, const uVec2 &Res, const ld_t time
+#define FRAGPARAMLIST RGBRef out, const uVec2 &fragcoord, const uVec2 &res, const ld_t time
 #define Frag [](FRAGPARAMLIST)
 
 typedef std::function<void(FRAGPARAMLIST)> FragShader;
